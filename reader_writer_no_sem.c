3ed958f7c456ec2b5bf3b4d76f2b3ce96458c5fb
김현재 <kimhun456@gmail.com>
2015-12-03 20:53:58 +0900

    asdf

--- reader_writer_no_sem.c
+++ reader_writer_no_sem.c
@@ -3,28 +3,19 @@
 #include <semaphore.h>
 #include <unistd.h>
 
-
-/*
-  변수들을 설정해준다.
-
-*/
+#define BOOKPAGES 10000
 #define READ_THREADS 2
 #define WRITE_THREADS 1
 
-
-/*
-  세마포어들을 설정한다.
-  read_mutex는 read를 하기 위해서 필요하다.
-  wirte_mutex는 writer가 쓰기 위해서 필요하다.
-
-*/
 sem_t read_mutex;
 sem_t write_mutex;
 
 
 int readCount=0;
 int count = 0;
+int books[BOOKPAGES];
 
+int currentPage = 0;
 
 void * Writer(void *arg)
 {
@@ -32,12 +23,17 @@ void * Writer(void *arg)
     while(1){
       sleep(1);
 
+      printf("Writer %d is try to enter\n",id);
       sem_wait(&write_mutex);
 
+      books[currentPage++] = count++;
 
-      printf( "Writer %d is writting  %d  \n", id, count++ +1);
+      printf( "Writer %d is writting  %d  \n",id,count);
+
+
       sem_post(&write_mutex);
 
+      printf("Writer %d is leaving \n",id);
     }
 
 }
@@ -54,6 +50,8 @@ void *Reader(void *arg)
     while(1){
         sleep(1);
 
+        printf("Reader %d is try to enter\n",id);
+
         sem_wait(&read_mutex);
         readCount++;
         if(readCount==1)
@@ -61,20 +59,24 @@ void *Reader(void *arg)
             sem_wait(&write_mutex);
 
 
+            printf("Reader %d is reading\n",id);
         }
         sem_post(&read_mutex);
 
 
         // read하는 부분!!!!
-        printf(" Reader %d's read data : %d \n", id, count);
+        printf(" Reader %d's read data : ",id);
+        for(i=0;i<count;i++){
+          printf("%3d" ,books[i]);
+        }
+        printf("\n");
 
         sem_wait(&read_mutex);
 
         readCount--;
-
-
         if(readCount==0)
         {
+            printf("Reader %d is leaving",id);
             sem_post(&write_mutex);
         }
         sem_post(&read_mutex);
@@ -83,6 +85,9 @@ void *Reader(void *arg)
 
 
 }
+
+
+
 
 
 int main()
@@ -94,6 +99,18 @@ int main()
 
     pthread_t Reader_threads[100];
     pthread_t Writer_threads[100];
+
+    // int a = 1;
+    // int b = 2;
+    //
+    // pthread_create(&Reader_threads[0],NULL,Reader,(void *)a);
+    // pthread_create(&Reader_threads[1],NULL,Reader,(void *)b);
+    // pthread_create(&Writer_threads[0],NULL,Writer,(void *)a);
+    //
+    // pthread_join(Reader_threads[0],NULL);
+    // pthread_join(Reader_threads[1],NULL);
+    // pthread_join(Reader_threads[0],NULL);
+
 
     for(i=0;i<READ_THREADS;i++)
     {
